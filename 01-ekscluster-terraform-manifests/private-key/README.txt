fa84ca7ac9227821f230c7cd64228fbdf8ef4882
Greg <gzak252@gmail.com>
2025-02-08 12:43:20 +0100

    Add eks cluster, ebs controller and app manifests

---
+++ 01-ekscluster-terraform-manifests/private-key/README.txt
@@ -0,0 +1,1 @@
+Make sure you put your private key pem file (devops.pem) in this directory
---
+++ 02-ebs-addon-terraform-manifests/c1-versions.tf
@@ -0,0 +1,25 @@
+# Terraform Settings Block
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      #version = ">= 4.65"
+      version = ">= 5.31"      
+     }
+  }
+  # Adding Backend as S3 for Remote State Storage
+  backend "s3" {
+    bucket = "eks-tfstate-bucket-gz"
+    key    = "ebs-addon/terraform.tfstate"
+    region = "eu-central-1" 
+
+    # For State Locking
+    dynamodb_table = "ebs-addon"    
+  }     
+}
+
+# Terraform Provider Block
+provider "aws" {
+  region = var.aws_region
+}
